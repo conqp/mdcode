de22d912c50a5c6f182433a854c83fc6d687430c
Richard Neumann <mail@richard-neumann.de>
2022-10-02 03:58:18 +0200

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -19,30 +19,35 @@ fn main() {
     args().skip(1).for_each(format)
 }
 
-fn format(file: impl AsRef<Path>) {
-    let file = file.as_ref();
-    match std::fs::read_to_string(file) {
-        Ok(text) => {
-            let extension = file
-                .extension()
-                .and_then(|ext| ext.to_str())
-                .unwrap_or_default();
-
-            let language = LANGUAGES.get(extension).unwrap_or(&"");
-            //FIXME: escape the text properly, or use indentation
-            match file.to_str() {
-                Some(filename) => {
-                    println!("`{}`:", filename);
-                    println!(
-                        "```{}\n{}{}```",
-                        language,
-                        text,
-                        if !text.ends_with('\n') { "\n" } else { "" }
-                    );
-                }
-                None => eprintln!("Could not extract file name."),
-            }
-        }
+pub fn format(file: impl AsRef<Path>) {
+    match std::fs::read_to_string(file.as_ref()) {
+        Ok(text) => print(file.as_ref(), &text),
         Err(code) => eprintln!("Error reading file: {}", code),
     }
+}
+
+fn print(file: &Path, text: &String) {
+    //FIXME: escape the text properly, or use indentation
+    match file.to_str() {
+        Some(filename) => {
+            println!("`{}`:", filename);
+            println!(
+                "```{}\n{}{}```",
+                language(file),
+                text,
+                if !text.ends_with('\n') { "\n" } else { "" }
+            );
+        }
+        None => eprintln!("Could not extract file name."),
+    }
+}
+
+fn language(file: &Path) -> &'static str {
+    LANGUAGES
+        .get(
+            file.extension()
+                .and_then(|ext| ext.to_str())
+                .unwrap_or_default(),
+        )
+        .unwrap_or(&"")
 }
