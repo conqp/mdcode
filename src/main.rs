32bcb1e8a27ecf3e82a1f133c0b603d6058517da
Richard Neumann <mail@richard-neumann.de>
2023-06-26 01:57:33 +0200

    Simplify code
    While at it, make formatting more stable by using indentation

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,31 @@
-use std::collections::HashMap;
 use std::env::args;
-use std::ffi::OsStr;
-use std::path::Path;
+use std::fs::read_to_string;
 
-use once_cell::sync::Lazy;
-
-static LANGUAGES: Lazy<HashMap<&'static str, &'static str>> = Lazy::new(|| {
-    HashMap::from([
-        ("c", "c"),
-        ("cpp", "cpp"),
-        ("h", "cpp"),
-        ("py", "python"),
-        ("rs", "rust"),
-        ("toml", "toml"),
-    ])
-});
+const INDENT: &str = "    ";
 
 fn main() {
-    args().skip(1).for_each(format);
+    args().skip(1).for_each(|s| format(&s));
 }
 
-pub fn format(file: impl AsRef<Path>) {
-    match std::fs::read_to_string(file.as_ref()) {
-        Ok(text) => print(file.as_ref(), &text),
-        Err(code) => eprintln!("Error reading file: {code}"),
-    }
-}
-
-fn print(file: &Path, text: &String) {
-    //FIXME: escape the text properly, or use indentation
-    file.to_str().map_or_else(
-        || eprintln!("Could not extract file name."),
-        |filename| {
-            println!("`{filename}`:");
-            println!(
-                "```{}\n{text}{}```",
-                language(file),
-                if text.ends_with('\n') { "" } else { "\n" }
-            );
+pub fn format(file: &str) {
+    read_to_string(file).map_or_else(
+        |_| {
+            eprintln!("Error reading file: {file}");
+        },
+        |text| {
+            println!("`{file}`\n\n{}", indent_lines(&text));
         },
     );
 }
 
-fn language(file: &Path) -> &'static str {
-    LANGUAGES
-        .get(file.extension().and_then(OsStr::to_str).unwrap_or_default())
-        .unwrap_or(&"")
+fn indent_lines(text: &str) -> String {
+    let mut out = String::new();
+
+    for line in text.lines() {
+        out.push_str(INDENT);
+        out.push_str(line);
+        out.push('\n');
+    }
+
+    out
 }
