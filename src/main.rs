10fac20800189b8a82cbd916a75b7a6d056f5b26
Richard Neumann <mail@richard-neumann.de>
2023-06-26 02:19:15 +0200

    Import names explicitly

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env::args;
 use std::fs::File;
-use std::io;
-use std::io::BufRead;
+use std::io::{self, BufRead, BufReader, Lines};
 use std::path::Path;
 
 const INDENT: &str = "    ";
@@ -25,10 +24,10 @@ pub fn format(file: &str) {
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+fn read_lines<P>(filename: P) -> io::Result<Lines<BufReader<File>>>
 where
     P: AsRef<Path>,
 {
     let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    Ok(BufReader::new(file).lines())
 }
