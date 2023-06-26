850f6d41d8bad83000a7994e2868ae16ea21d7c2
Richard Neumann <mail@richard-neumann.de>
2023-06-26 02:17:51 +0200

    Refactor line processing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::env::args;
-use std::fs::read_to_string;
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::path::Path;
 
 const INDENT: &str = "    ";
 
@@ -8,24 +11,24 @@ fn main() {
 }
 
 pub fn format(file: &str) {
-    read_to_string(file).map_or_else(
-        |_| {
-            eprintln!("Error reading file: {file}");
-        },
-        |text| {
-            println!("`{file}`\n\n{}", indent_lines(&text));
+    println!("\n`{file}`\n");
+
+    read_lines(file).map_or_else(
+        |error| eprintln!("could not read file: {error}"),
+        |lines| {
+            lines.map_while(Result::ok).for_each(|line| {
+                println!("{INDENT}{line}");
+            });
         },
     );
 }
 
-fn indent_lines(text: &str) -> String {
-    let mut out = String::new();
-
-    for line in text.lines() {
-        out.push_str(INDENT);
-        out.push_str(line);
-        out.push('\n');
-    }
-
-    out
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
